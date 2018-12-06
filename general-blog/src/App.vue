3d9cf71df04221f30475ec74328d0ce641fe55b6
wwilll <first94@outlook.com>
2018-12-06 21:09:12 +0800

    Submit the vue-cli3 test project for the first time

---
+++ general-blog/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Hello World!</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+}
+</style>
---
+++ general-blog/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>parent's params:{{ msg }}</h1>
+    <p>I'm HelloWorld Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.hello {
+  border: 1px solid red;
+  background-color: rgb(206, 194, 194);
+  h1 {
+    background-color: skyblue;
+  }
+}
+
+</style>
